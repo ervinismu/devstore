d8a2fbfec11e13ebf8fa0275b6f9ebed07eaa675
ervinismu <social.ervin@gmail.com>
2023-04-08 20:36:14 +0700

    fix : change logic update and delete category.

--- internal/app/repository/interface_repository.go
+++ internal/app/repository/interface_repository.go
@@ -5,7 +5,7 @@ import "github.com/ervinismu/devstore/internal/app/model"
 type ICategoryRepository interface {
 	Create(category model.Category) error
 	Browse() ([]model.Category, error)
+	Update(category model.Category) error
 	GetByID(id string) (model.Category, error)
-	UpdateByID(id string, category model.Category) error
 	DeleteByID(id string) error
 }
